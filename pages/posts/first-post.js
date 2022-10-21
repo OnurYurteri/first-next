126c4bae1e3ea6c4734e69254377cc0ab8e75dc4
Onur Yurteri <onur.yurteri@scalingfunds.com>
2022-10-22 02:31:01 +0300

    feat: add layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,18 +1,18 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Image from 'next/image';
+import Image from 'next/image'
 
 import { AnalyticsBrowser } from '@segment/analytics-next'
 
 
 const YourComponent = () => (
-    <Image
-        src="/images/profile.png" // Route of the image file
-        height={144} // Desired size with correct aspect ratio
-        width={144} // Desired size with correct aspect ratio
-        alt="Your Name"
-    />
-);
+  <Image
+    src="/images/profile.png" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+)
 
 export default function FirstPost() {
   const analytics = AnalyticsBrowser.load({ writeKey: 'AWMBqiU0iLuFhbXDL4Ba3POuJOyzQlkg' })
@@ -20,14 +20,16 @@ export default function FirstPost() {
 
   return (
     <>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <h1>First Post</h1>
-      <YourComponent />
-      <h2>
-        <Link href="/">Go back⏪</Link>
-      </h2>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+        <YourComponent />
+        <h2>
+          <Link href="/">Go back⏪</Link>
+        </h2>
+      </Layout>
     </>
   )
 }
