0e90f670ed5deae56a58cc9edacd7e73185eadb6
Onur Yurteri <onur.yurteri@scalingfunds.com>
2022-10-22 14:02:10 +0300

    chore: remove `debugger`

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import Analytics from '../core/analytics'
 
 export default function App({ Component, pageProps }) {
-  debugger
   return (
     <Analytics Component={Component} pageProps={pageProps}>
       <Component {...pageProps} />
